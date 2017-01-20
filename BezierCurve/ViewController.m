8af1b2a2b7a6020f4d1d50aff4edd05967a0cd70
lvyingliang200 <475478714@qq.com>
2017-01-20 12:12:43 +0800

    Animation2

--- BezierCurve/ViewController.m
+++ BezierCurve/ViewController.m
@@ -26,33 +26,23 @@
     self.shapeLayer    = [CAShapeLayer layer];
     self.progressLayer = [CAShapeLayer layer];
     CGRect drawBounds = CGRectMake(0, 0, 100, 100);
-    [self updateLayer:self.shapeLayer Bounds:drawBounds StrockColor:[UIColor redColor]];
-    [self updateLayer:self.progressLayer Bounds:drawBounds StrockColor:[UIColor greenColor]];
-    _timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(circleAnimation) userInfo:nil repeats:YES];
-}
-
-- (void)circleAnimation
-{
-    CGFloat redSpeed   = 0.1;
-    CGFloat greenSpeed = redSpeed/2;
+    [self updateLayer:self.shapeLayer Bounds:drawBounds StrockColor:[UIColor redColor] Start:0.75 End:0.75];
+    [self updateLayer:self.progressLayer Bounds:drawBounds StrockColor:[UIColor greenColor] Start:0 End:0];
     
-    if (_shapeLayer.strokeStart == 0 && (_progressLayer.strokeEnd != 0.8 || _shapeLayer.strokeEnd != 1)) {
-        if (_shapeLayer.strokeEnd >= 1) {
-            redSpeed = 0;
-        }
-        if (_progressLayer.strokeEnd >= 0.8) {
-            greenSpeed = 0;
-        }
-        self.shapeLayer.strokeEnd += redSpeed;
-        self.progressLayer.strokeEnd += greenSpeed;
-    }else {
-        [_timer invalidate];
-    }
+    
+    _timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(circleAnimationTwo) userInfo:nil repeats:YES];
 }
 
-- (void)updateLayer:(CAShapeLayer *)layer Bounds:(CGRect)bounds StrockColor:(UIColor *)strockColor
+/**
+ 设置layer的香菇你属性
+ 
+ @param layer       ：要进行操作的CAShapeLayer
+ @param bounds      ：layer的大小
+ @param strockColor ：划线的颜色
+ */
+- (void)updateLayer:(CAShapeLayer *)layer Bounds:(CGRect)bounds StrockColor:(UIColor *)strockColor Start:(CGFloat)start End:(CGFloat)end
 {
-
+    
     layer.frame       = bounds;
     layer.position    = self.view.center;
     layer.fillColor   = [UIColor clearColor].CGColor;
@@ -64,6 +54,40 @@
     layer.path        = circlePath.CGPath;
     [self.view.layer addSublayer:layer];
 }
+/**
+    扇形动画1
+ */
+- (void)circleAnimationOne
+{
+    CGFloat speed = 0.05;
+    if (_progressLayer.strokeEnd <=0.75) {
+        _shapeLayer.strokeEnd += speed;
+        _progressLayer.strokeEnd += speed;
+    }else if (_shapeLayer.strokeEnd <=1){
+        _shapeLayer.strokeEnd += speed;
+    }else{
+        [_timer invalidate];
+    }
+}
+/**
+    扇形动画2
+ */
+- (void)circleAnimationTwo
+{
+    CGFloat redSpeed   = 0.1;
+    CGFloat greenSpeed = redSpeed/2;
+    if (_progressLayer.strokeEnd <=0.75) {
+        _shapeLayer.strokeEnd += redSpeed;
+        _progressLayer.strokeEnd += greenSpeed;
+    }else if (_shapeLayer.strokeEnd <=1){
+        _shapeLayer.strokeEnd += redSpeed;
+    }else{
+        [_timer invalidate];
+    }
+}
+/**
+    释放Timer
+ */
 - (void)dealloc {
     self.timer = nil;
 }
